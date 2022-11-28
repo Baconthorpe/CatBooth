314ac4955ee6c3dddbba03b881a61af9824bdd9d
Zeke Abuhoff <zabuhoff@gmail.com>
2022-11-28 12:47:54 -0800

    Initial Commit

---
+++ CatBooth/CatBoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatBoothApp.swift
+//  CatBooth
+//
+//  Created by Ezekiel Abuhoff on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct CatBoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
